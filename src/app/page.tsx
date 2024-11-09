7bc0ba853598aa5b3bde057daba763facbad918c
jhon <jhon.bocanegra26@gmail.com>
2024-11-09 00:49:03 -0500

    Update README, add global styles, and implement font source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, quasi
-        ipsum alias, quas corrupti perspiciatis reprehenderit maiores labore
-        voluptas aperiam dignissimos perferendis quia, explicabo eveniet
-        corporis consequatur iusto nihil fugit quod non saepe. Autem minus quis
-        reprehenderit eaque natus harum?
+        Hola mundo!
       </h1>
     </div>
   )
